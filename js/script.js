3e791ac16eef16f55b714b5291b40dd6d5597306
Jawad <jawadnasiri2222@gmail.com>
2025-03-16 01:50:00 +0100

    Add data list template to the project

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ addSetBtn.onclick = () => {
     setGroupElement.innerHTML =
     `
     <span>Set ${++counter}:</span>
-    <input type="number" name="reps" min="1" placeholder="Reps" required>
     <input type="number" name="weight" min="1" step="0.5" placeholder="Weight (kg)" required>
+    <input type="number" name="reps" min="1" placeholder="Reps" required>
     
     `;
     if(setGroupElement) {
@@ -22,4 +22,16 @@ addSetBtn.onclick = () => {
         successMessage.style.display = 'none';
     }, 2000)
     setGroupContainer.append(setGroupElement);
+}
+
+
+document.querySelector('button[type="submit"]').onclick = (evt) => {
+    evt.preventDefault()
+    let inputExerciseValue = document.querySelector('input[name="exercise"]').value
+    // console.log(inputExerciseValue)
+
+    if(inputExerciseValue.toLowerCase() === "back") {
+        document.querySelector('form').action = 'back.php'
+        console.log(document.querySelector('form').action)
+    }
 }
